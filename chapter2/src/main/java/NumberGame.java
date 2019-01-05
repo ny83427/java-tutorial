fd5f9ef66357e19ce40bd13d9adf921894fe6563
Nathanael Yang <ny83427@gmail.com>
2019-01-04 16:58:44 -0800

    new line format adjust

--- chapter2/src/main/java/NumberGame.java
+++ chapter2/src/main/java/NumberGame.java
@@ -3,25 +3,25 @@ public class NumberGame {
     private static void scanMaxAndMin(int[] numbers) {
         int max = 0, min = 0;
         // Your stuff here
-        System.out.printf("Max is %d and min is %d", max, min);
+        System.out.printf("Max is %d and min is %d%n", max, min);
     }
 
     private static void scanFirstTwoMaxs(int[] numbers) {
         int max = 0, secondMax = 0;
         // Your stuff here
-        System.out.printf("Max is %d and second max is %d", max, secondMax);
+        System.out.printf("Max is %d and second max is %d%n", max, secondMax);
     }
 
     private static void scanFirstThreeMaxs(int[] numbers) {
         int max = 0, secondMax = 0, thirdMax = 0;
         // Your stuff here
-        System.out.printf("Max is %d, second max is %d and third max is %d", max, secondMax, thirdMax);
+        System.out.printf("Max is %d, second max is %d and third max is %d%n", max, secondMax, thirdMax);
     }
 
     private static void threeSums(int[] numbers) {
         int count = 0;
         // Your stuff here
-        System.out.printf("There are %d combinations of 3 numbers that sum of them is 0", count);
+        System.out.printf("There are %d combinations of 3 numbers that sum of them is 0%n", count);
     }
 
     public static void main(String[] args) {
