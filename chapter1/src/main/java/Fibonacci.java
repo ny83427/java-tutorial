6be79d79fcfa96f20c30582a3faf3c557b1fe6d3
Nathanael Yang <ny83427@gmail.com>
2019-01-07 22:05:35 -0800

    chapter 1 solution initial commit

--- chapter1/src/main/java/Fibonacci.java
+++ chapter1/src/main/java/Fibonacci.java
@@ -1,15 +1,94 @@
-public class Fibonacci {
+import java.math.BigInteger;
+import java.util.Scanner;
 
-    private int calc(int n) {
-        // You stuff here
-        return n;
+/**
+ * <pre>
+ * Implementation notes:
+ * 1. Check argument: is there any restriction and proper handling?
+ * 2. Corner case: will the result become incorrect in certain circumstances?
+ * 3. Performance tuning: where is the hotpoint and how to fix it?
+ * 4. Code style: is your code formatted clearly and organized purposely? are the log messages helpful?
+ * 5. More considerations: thread safe? message internationalization?
+ * </pre>
+ *
+ * @author Nathanael Yang
+ */
+public class Fibonacci {
+    private int lastCacheIndex = -1;
+
+    private BigInteger[] cache = new BigInteger[153];
+
+    private void validateArgument(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("Only positive number is meaningful to find " +
+                "corresponding Fibonacci number. However, your input is " + n);
+        }
+    }
+
+    BigInteger calc(int n) {
+        this.validateArgument(n);
+
+        // initialize the first and second number in cache
+        if (lastCacheIndex == -1) {
+            cache[0] = cache[1] = BigInteger.ONE;
+            lastCacheIndex = 1;
+        }
+
+        // check capacity and resize array if necessary
+        if (n > cache.length) {
+            BigInteger[] newCache = new BigInteger[Math.max(cache.length * 2, n)];
+            System.arraycopy(cache, 0, newCache, 0, lastCacheIndex + 1);
+            cache = newCache;
+        }
+
+        // return cached result if hit
+        if (cache[n - 1] != null) {
+            return cache[n - 1];
+        }
+
+        // built cache from last cached index on to avoid duplicated calculation
+        for (int i = lastCacheIndex + 1; i < n; i++) {
+            cache[i] = cache[i - 1].add(cache[i - 2]);
+        }
+        lastCacheIndex = n - 1;
+
+        return cache[n - 1];
+    }
+
+    private BigInteger calcNaively(int n) {
+        this.validateArgument(n);
+        return n <= 2 ? BigInteger.ONE : (calcNaively(n - 1).add(calcNaively(n - 2)));
+    }
+
+    private static final long[] TIME_RANGES = {1000 * 60 * 60, 1000 * 60, 1000};
+    private static final String[] TIME_LABELS = {"hours", "minutes", "seconds"};
+
+    private static String formatElapsedTime(long elapsed) {
+        for (int i = 0; i < TIME_RANGES.length; i++) {
+            if (elapsed >= TIME_RANGES[i]) {
+                return String.format("%.2f %s", elapsed * 1.0f / TIME_RANGES[i], TIME_LABELS[i]);
+            }
+        }
+
+        return elapsed + " ms";
     }
 
     public static void main(String[] args) {
         Fibonacci fib = new Fibonacci();
-        System.out.println(fib.calc(3));
-        System.out.println(fib.calc(20));
-        System.out.println(fib.calc(100));
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            int n = scanner.nextInt();
+            boolean useOptimized = scanner.next().toUpperCase().startsWith("O");
+            String method = useOptimized ? "Optimized" : "Naive";
+            final long start = System.currentTimeMillis();
+            try {
+                BigInteger result = useOptimized ? fib.calc(n) : fib.calcNaively(n);
+                System.out.printf("Fibonacci number of %d is %s. %s calculation spent %s.%n",
+                    n, result, method, formatElapsedTime(System.currentTimeMillis() - start));
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
---
+++ chapter1/src/test/java/FibonacciTest.java
@@ -0,0 +1,55 @@
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.math.BigInteger;
+import java.time.Duration;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class FibonacciTest {
+
+    private final Fibonacci fibonacci = new Fibonacci();
+
+    @Test
+    void calcWithZero() {
+        assertThrows(IllegalArgumentException.class, () -> fibonacci.calc(0));
+    }
+
+    @Test
+    void calcWithNegativeNumber() {
+        assertThrows(IllegalArgumentException.class, () -> fibonacci.calc(-10));
+    }
+
+    @Test
+    void performance() {
+        assertTimeout(Duration.ofMillis(100),
+            () -> assertEquals(new BigInteger("354224848179261915075"), fibonacci.calc(100)));
+        assertTimeout(Duration.ofMillis(100),
+            () -> assertEquals(new BigInteger("280571172992510140037611932413038677189525"), fibonacci.calc(200)));
+    }
+
+    @Test
+    void calc() {
+        assertEquals(fibonacci.calc(1), BigInteger.ONE);
+        assertEquals(fibonacci.calc(2), BigInteger.ONE);
+        assertEquals(fibonacci.calc(10), new BigInteger("55"));
+        assertEquals(fibonacci.calc(50), new BigInteger("12586269025"));
+        assertEquals(fibonacci.calc(200), new BigInteger("280571172992510140037611932413038677189525"));
+    }
+
+    @Test
+    @DisplayName("access level was hacked but performance sucks\uD83D\uDE2D")
+    void calcNaively() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = Fibonacci.class.getDeclaredMethod("calcNaively", int.class);
+        method.setAccessible(true);
+
+        assertEquals(method.invoke(fibonacci, 10), new BigInteger("55"));
+        assertEquals(method.invoke(fibonacci, 40), new BigInteger("102334155"));
+        assertEquals(method.invoke(fibonacci, 42), new BigInteger("267914296"));
+    }
+
+}
