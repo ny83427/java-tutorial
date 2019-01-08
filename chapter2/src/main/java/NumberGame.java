569165db13707289e08046a4acc9cfa713134406
Nathanael Yang <ny83427@gmail.com>
2019-01-08 10:52:30 -0800

    :bulb: chapter 2 solution improvements
    1. comments samples: single line, javadoc;
    2. unit tests: basic case design;
    3. duplicated codes removal;
    4. three sums algorithm explanations.

--- chapter2/src/main/java/NumberGame.java
+++ chapter2/src/main/java/NumberGame.java
@@ -1,13 +1,45 @@
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
+/**
+ * Solution of Chapter 2 Assignments
+ *
+ * @author Nathanael Yang  2019/01/08 10:09 AM
+ */
 public class NumberGame {
+
+    /**
+     * validate a given integer array to check whether there are at least certain count of elements
+     *
+     * @param nums  the integer array
+     * @param minLength minimum length of the integer array
+     * @throws IllegalArgumentException if <code>minLength</code> is non-positive,
+     *  or the array is <code>null</code>, or has less than <code>minLength</code> elements
+     */
     private static void validateArray(int[] nums, int minLength) {
+        if (minLength <= 0) {
+            throw new IllegalArgumentException("Minimum array length " + minLength + " is not a positive integer");
+        }
         if (nums == null || nums.length < minLength) {
             throw new IllegalArgumentException("Please provide an array with " + minLength + " integers at least");
         }
     }
 
-    private static void scanMaxAndMin(int[] numbers) {
+    /**
+     * <pre>
+     * scan a given integer array and find the maximum, minimum number
+     * the purpose of this exercise if about control flow, statements, logical expressions
+     * and how to test your code in normal cases and corner cases
+     * </pre>
+     *
+     * @param numbers the integer array
+     * @return an integer array with max first and min second
+     * @throws IllegalArgumentException if given integer array is <code>null</code> or empty
+     */
+    static int[] scanMaxAndMin(int[] numbers) {
         validateArray(numbers, 1);
 
         int max = numbers[0], min = numbers[0];
@@ -15,10 +47,19 @@ public class NumberGame {
             if (num > max) max = num;
             if (num < min) min = num;
         }
-        System.out.printf("Max is %d and min is %d%n", max, min);
+        System.out.printf("Max of %s is %d and min is %d%n", Arrays.toString(numbers), max, min);
+        // Range<T extends Comparable> range = new Range<>(min, max);
+        return new int[]{max, min};
     }
 
-    private static void scanFirstTwoMaxs(int[] numbers) {
+    /**
+     * scan a given integer array and find the maximum, second maximum number
+     *
+     * @param numbers the integer array
+     * @return an integer array of max and second max number
+     * @throws IllegalArgumentException if given integer array is <code>null</code> or less than 2 elements
+     */
+    static int[] scanFirstTwoMaxs(int[] numbers) {
         validateArray(numbers, 2);
 
         int max = Integer.MIN_VALUE, secondMax = Integer.MIN_VALUE;
@@ -30,10 +71,18 @@ public class NumberGame {
                 secondMax = num;
             }
         }
-        System.out.printf("Max is %d and second max is %d%n", max, secondMax);
+        System.out.printf("Max of %s is %d and second max is %d%n", Arrays.toString(numbers), max, secondMax);
+        return new int[]{max, secondMax};
     }
 
-    private static void scanFirstThreeMaxs(int[] numbers) {
+    /**
+     * scan a given integer array and find the maximum, second maximum, and third maximum number
+     *
+     * @param numbers the integer array
+     * @return an integer array of max, second max, and third max number
+     * @throws IllegalArgumentException if given integer array is <code>null</code> or less than 3 elements
+     */
+    static int[] scanFirstThreeMaxs(int[] numbers) {
         validateArray(numbers, 3);
 
         int max = Integer.MIN_VALUE, secondMax = Integer.MIN_VALUE, thirdMax = Integer.MIN_VALUE;
@@ -49,84 +98,85 @@ public class NumberGame {
                 thirdMax = num;
             }
         }
-        System.out.printf("Max is %d, second max is %d and third max is %d%n", max, secondMax, thirdMax);
-    }
-
-    static int threeSumsBruteForce(int[] numbers) {
-        final long start = System.currentTimeMillis();
-
-        int count = 0;
-        boolean printed = false;
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                for (int k = j + 1; k < numbers.length; k++) {
-                    if (numbers[i] + numbers[j] + numbers[k] == 0) {
-                        if (count < 100) {
-                            System.out.println(String.format("%d + %d + %s = 0", numbers[i], numbers[j], numbers[k]));
-                        } else if (!printed) {
-                            System.out.println("......");
-                            printed = true;
-                        }
-                        count++;
-                    }
-                }
-            }
-        }
-        System.out.println(String.format("%d combinations of 3 nums sum of which is zero. Scan %d numbers in %sMS%n",
-            count, numbers.length, System.currentTimeMillis() - start));
-        return count;
-    }
-
-    static int threeSums(int[] numbers) {
-        final long start = System.currentTimeMillis();
-
-        int count = 0;
-        boolean printed = false;
-        Arrays.sort(numbers);
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                int k = Arrays.binarySearch(numbers, j + 1, numbers.length, -numbers[i] - numbers[j]);
-                if (k >= 0) {
-                    if (count < 100) {
-                        System.out.println(String.format("%d + %d + %s = 0", numbers[i], numbers[j], numbers[k]));
-                    } else if (!printed) {
-                        System.out.println("......");
-                        printed = true;
-                    }
-                    count++;
-                }
-            }
-        }
-        System.out.println(String.format("%d combinations of 3 nums sum of which is zero. Scan %d numbers in %sMS%n",
-            count, numbers.length, System.currentTimeMillis() - start));
-        return count;
+        System.out.printf("Max of %s is %d, second max is %d and third max is %d%n",
+            Arrays.toString(numbers), max, secondMax, thirdMax);
+        return new int[]{max, secondMax, thirdMax};
     }
 
     /**
-     * https://leetcode.com/problems/3sum/discuss/7399/Easiest-Java-Solution
+     * Brute force strategy to find combinations of three sums sum of them is zero
+     * Time complexity O(n^3), which can be improved to O(n^2 * log(n))
      */
-    static int threeSumsSupportDuplicated(int[] nums) {
-        final long start = System.currentTimeMillis();
+    static List<int[]> threeSumsBruteForce(int[] nums) {
+        if (nums == null || nums.length < 3) return null;
 
-        int count = 0;
-        Arrays.sort(nums);
-        boolean printed = false;
-        for (int i = 0; i + 2 < nums.length; i++) {
-            // skip same result
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
+        List<int[]> list = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        list.add(new int[]{nums[i], nums[j], nums[k]});
+                    }
+                }
             }
+        }
+        return list;
+    }
+
+    /**
+     * Sort the array first, then using binary search to gain speed improvement<br/>
+     * Time complexity O(n^2 * log(n)), certain tips such as early exit will also help
+     */
+    static List<int[]> threeSums(int[] nums) {
+        if (nums == null || nums.length < 3) return null;
+
+        List<int[]> list = new ArrayList<>();
+        // Keep track of each indexes, once they are found their status will change
+        int[] indicators = new int[nums.length];
+        Arrays.fill(indicators, -1);
+
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++) {
+            // early exit
+            if (nums[i] > 0) break;
+
+            for (int j = i + 1; j < nums.length; j++) {
+                int k = Arrays.binarySearch(nums, j + 1, nums.length, -nums[i] - nums[j]);
+                if (k >= 0 && indicators[i] == -1 && indicators[j] == -1 && indicators[k] == -1) {
+                    list.add(new int[]{nums[i], nums[j], nums[k]});
+                    indicators[i] = indicators[j] = indicators[k] = 1;
+                }
+            }
+        }
+        return list;
+    }
+
+    /**
+     * <pre>
+     * https://leetcode.com/problems/3sum/discuss/7399/Easiest-Java-Solution
+     *
+     * Attention: this solution only accept unique combinations, which is not exactly same to the context of
+     * our number game. For example, [-2, -2, -1, -1, 3, 3, 4] should return 2 as per our requirement, but
+     * this approach will return 1 since [-2, -1, 3] occurred twice
+     * </pre>
+     */
+    static List<int[]> uniqueThreeSums(int[] nums) {
+        if (nums == null || nums.length < 3) return null;
+
+        List<int[]> list = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i + 2 < nums.length; i++) {
+            // since array is sorted, there is no way that the rest integers sum of which can be zero
+            if (nums[i] > 0) break;
+
+            // skip same result
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
             int j = i + 1, k = nums.length - 1;
             int target = -nums[i];
             while (j < k) {
                 if (nums[j] + nums[k] == target) {
-                    if (count < 100) {
-                        System.out.println(String.format("%d + %d + %s = 0", nums[i], nums[j], nums[k]));
-                    } else if (!printed) {
-                        System.out.println("......");
-                        printed = true;
-                    }
-                    count++;
+                    list.add(new int[]{nums[i], nums[j], nums[k]});
 
                     j++;
                     k--;
@@ -140,23 +190,43 @@ public class NumberGame {
                 }
             }
         }
-
-        System.out.println(String.format("%d combinations of 3 nums sum of which is zero. Scan %d numbers in %sMS%n",
-            count, nums.length, System.currentTimeMillis() - start));
-        return count;
+        return list;
     }
 
-    public static void main(String[] args) {
-        int[] numbers = {-1, -2, 3, -4, -5, 9, 10, 11};
-        scanMaxAndMin(numbers);
-        scanFirstTwoMaxs(numbers);
-        scanFirstThreeMaxs(numbers);
+    public static void main(String[] args) throws NoSuchMethodException,
+        InvocationTargetException, IllegalAccessException {
+        int[][] numbers = {
+            {-1, -2, 3, -4, -5, 9, 10, 11},
+            {-1, -1, -2, -2, 3, -4, -5, 9, 8, 4}
+        };
+        for (int[] nums : numbers) {
+            scanMaxAndMin(nums);
+            scanFirstTwoMaxs(nums);
+            scanFirstThreeMaxs(nums);
+        }
 
-        threeSumsBruteForce(numbers);
-        threeSums(numbers);
+        // using reflection to reduce duplicated codes for troubleshooting purpose
+        // students can simply ignore implementation details but just get the motivation
+        Method[] methods = {
+            NumberGame.class.getDeclaredMethod("threeSumsBruteForce", int[].class),
+            NumberGame.class.getDeclaredMethod("threeSums", int[].class),
+            NumberGame.class.getDeclaredMethod("uniqueThreeSums", int[].class),
+        };
 
-        threeSumsBruteForce(new int[] {-1, -1, -2, -2, 3, -4, -5, 9, 10, 11});
-        threeSums(new int[] {-1, -1, -2, -2, 3, -4, -5, 9, 10, 11});
+        int maxDisplayCount = 100;
+        for (int[] nums : numbers) {
+            for (Method method : methods) {
+                final long start = System.currentTimeMillis();
+                @SuppressWarnings("unchecked")
+                List<int[]> list = (List<int[]>) method.invoke(null, nums);
+                for (int i = 0; i < Math.min(maxDisplayCount, list.size()); i++) {
+                    int[] arr = list.get(i);
+                    System.out.println(String.format("%d + %d + %d = 0", arr[0], arr[1], arr[2]));
+                }
+                System.out.println(String.format("%d combinations of 3 nums found by `%s`. Scan %d numbers in %sMS%n",
+                    list.size(), method.getName(), nums.length, System.currentTimeMillis() - start));
+            }
+        }
     }
 
 }
--- chapter2/src/test/java/NumberGameTest.java
+++ chapter2/src/test/java/NumberGameTest.java
@@ -1,6 +1,4 @@
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 
 import java.io.IOException;
@@ -9,7 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(Lifecycle.PER_CLASS)
 class NumberGameTest {
@@ -17,6 +15,8 @@ class NumberGameTest {
 
     @BeforeAll
     void init() {
+        // These test files belong to Robert Sedgewick and Kevin Wayne of Princeton University
+        // https://algs4.cs.princeton.edu/14analysis/ThreeSum.java.html
         thousandInts = buildArray("1Kints.txt");
         twoThousandInts = buildArray("2Kints.txt");
         eightThousandInts = buildArray("8Kints.txt");
@@ -33,29 +33,77 @@ class NumberGameTest {
     }
 
     @Test
-    void threeSumsWithDuplicates() {
+    void scanMaxAndMin() {
+        assertThrows(IllegalArgumentException.class, () -> NumberGame.scanMaxAndMin(null));
+        assertThrows(IllegalArgumentException.class, () -> NumberGame.scanMaxAndMin(new int[]{}));
+
+        assertArrayEquals(new int[]{6, 1}, NumberGame.scanMaxAndMin(new int[]{1, 2, 3, 4, 5, 6}));
+        assertArrayEquals(new int[]{6, 1}, NumberGame.scanMaxAndMin(new int[]{6, 5, 4, 3, 2, 1}));
+        assertArrayEquals(new int[]{1, 1}, NumberGame.scanMaxAndMin(new int[]{1}));
+        assertArrayEquals(new int[]{1, 1}, NumberGame.scanMaxAndMin(new int[]{1, 1, 1, 1}));
+    }
+
+    @Test
+    void scanFirstTwoMaxs() {
+        assertThrows(IllegalArgumentException.class, () -> NumberGame.scanFirstTwoMaxs(null));
+        assertThrows(IllegalArgumentException.class, () -> NumberGame.scanFirstTwoMaxs(new int[]{}));
+        assertThrows(IllegalArgumentException.class, () -> NumberGame.scanFirstTwoMaxs(new int[]{1}));
+
+        assertArrayEquals(new int[]{3, 2}, NumberGame.scanFirstTwoMaxs(new int[]{1, 2, 3}));
+        assertArrayEquals(new int[]{3, 2}, NumberGame.scanFirstTwoMaxs(new int[]{3, 2, 1}));
+        assertArrayEquals(new int[]{1, 1}, NumberGame.scanFirstTwoMaxs(new int[]{1, 1, 1, 1}));
+    }
+
+    @Test
+    void scanFirstThreeMaxs() {
+        assertThrows(IllegalArgumentException.class, () -> NumberGame.scanFirstThreeMaxs(null));
+        assertThrows(IllegalArgumentException.class, () -> NumberGame.scanFirstThreeMaxs(new int[]{}));
+        assertThrows(IllegalArgumentException.class, () -> NumberGame.scanFirstThreeMaxs(new int[]{1}));
+        assertThrows(IllegalArgumentException.class, () -> NumberGame.scanFirstThreeMaxs(new int[]{1, 2}));
+
+        assertArrayEquals(new int[]{3, 2, 1}, NumberGame.scanFirstThreeMaxs(new int[]{1, 2, 3}));
+        assertArrayEquals(new int[]{6, 5, 4}, NumberGame.scanFirstThreeMaxs(new int[]{6, 5, 4, 3, 2, 1}));
+        assertArrayEquals(new int[]{1, 1, 1}, NumberGame.scanFirstThreeMaxs(new int[]{1, 1, 1, 1}));
+        assertArrayEquals(new int[]{1, -1, -2}, NumberGame.scanFirstThreeMaxs(new int[]{-1, -2, -3, 1}));
+    }
+
+    @Test
+    void uniqueThreeSums() {
         int[] nums = {-1, -1, -2, -2, 3, -4, -5, 9, 10, 11};
-        assertEquals(2, NumberGame.threeSumsSupportDuplicated(nums));
-        assertEquals(2, NumberGame.threeSums(nums));
-        assertEquals(2, NumberGame.threeSumsBruteForce(nums));
+        assertEquals(2, NumberGame.uniqueThreeSums(nums).size());
+
+        nums = new int[]{-1, -1, -2, -2, 3, 3, -4, -5, 9, 10, 11};
+        assertEquals(2, NumberGame.uniqueThreeSums(nums).size());
     }
 
     @Test
-    void threeSums() {
-        NumberGame.threeSums(thousandInts);
-        NumberGame.threeSumsBruteForce(twoThousandInts);
-        NumberGame.threeSumsBruteForce(eightThousandInts);
+    void threeSumsWithoutDuplicates() {
+        assertEquals(70, NumberGame.threeSums(thousandInts).size());
+        assertEquals(528, NumberGame.threeSums(twoThousandInts).size());
     }
 
     @Test
+    void threeSumsBruteForceWithoutDuplicates() {
+        assertEquals(70, NumberGame.threeSumsBruteForce(thousandInts).size());
+        assertEquals(528, NumberGame.threeSumsBruteForce(twoThousandInts).size());
+    }
+
+    @Test
+    void uniqueThreeSumsWithoutDuplicates() {
+        assertEquals(70, NumberGame.uniqueThreeSums(thousandInts).size());
+        assertEquals(528, NumberGame.uniqueThreeSums(twoThousandInts).size());
+    }
+
+    @Disabled("It's not that good to be a millionaire\uD83D\uDE05")
     void performance() {
         NumberGame.threeSums(millionInts);
+        NumberGame.threeSums(eightThousandInts);
     }
 
     @Test
-    void threeSumsBruteForce() {
-        NumberGame.threeSumsBruteForce(thousandInts);
-        NumberGame.threeSumsBruteForce(twoThousandInts);
-        NumberGame.threeSumsBruteForce(eightThousandInts);
+    void threeSumWithDuplicates() {
+        assertEquals(3, NumberGame.threeSums(new int[]{-1, -1, -2, -2, 3, 3, -4, -5, 9, 10, 11}).size());
+        assertEquals(2, NumberGame.threeSums(new int[]{-1, -1, -2, -2, 3, 4, -4, -5, 9, 10, 11}).size());
     }
+
 }
