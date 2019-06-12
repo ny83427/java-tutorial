afd1532de1d2f194ed56e6f621969ba7b8193628
Nathanael Yang <ny83427@gmail.com>
2019-06-12 09:43:42 -0700

    remove PVS-Studio comments since I will not use it for this project anymore

--- chapter2/src/main/java/NumberGame.java
+++ chapter2/src/main/java/NumberGame.java
@@ -1,7 +1,3 @@
-/*
-* This is a personal academic project. Dear PVS-Studio, please check it.
-* PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
-*/
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
